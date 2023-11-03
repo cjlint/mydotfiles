d7eace9bac40a9a84792dbc8896ae8b8d86743fe
CJ Linton <clinton.2742@gmail.com>
2023-11-03 14:35:31 -0400

    Add handlebars prettierd formatter, add stylua formatter

--- nvim/formatter.lua
+++ nvim/formatter.lua
@@ -1,15 +1,26 @@
-local eslint_d = require('formatter.defaults.eslint_d')
-local prettierd = require('formatter.defaults.prettierd')
+local eslint_d = require("formatter.defaults.eslint_d")
 
-require('formatter').setup({
-  logging = true,
-  log_level = vim.log.levels.WARN,
-  filetype = {
-    typescript = {eslint_d},
-    typescriptreact = {eslint_d},
-    javascript = {eslint_d},
-    javascriptreact = {eslint_d},
-    graphql = {eslint_d},
-    ["*"] = {prettierd}
-  }
+require("formatter").setup({
+	logging = true,
+	log_level = vim.log.levels.WARN,
+	filetype = {
+		typescript = { eslint_d },
+		typescriptreact = { eslint_d },
+		javascript = { eslint_d },
+		javascriptreact = { eslint_d },
+		graphql = { eslint_d },
+		handlebars = {
+			function()
+				return {
+					exe = "prettierd",
+					args = { "--parser", "glimmer" },
+					stdin = true,
+				}
+			end,
+		},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+		["*"] = { require("formatter.defaults.prettierd") },
+	},
 })
--- nvim/lint.lua
+++ nvim/lint.lua
@@ -1,7 +1,7 @@
-require('lint').linters_by_ft = {
-  typescript = {'eslint_d',},
-  typescriptreact = {'eslint_d',},
-  javascript = {'eslint_d',},
-  javascriptreact = {'eslint_d',},
-  graphql = {'eslint_d',},
+require("lint").linters_by_ft = {
+	typescript = { "eslint_d" },
+	typescriptreact = { "eslint_d" },
+	javascript = { "eslint_d" },
+	javascriptreact = { "eslint_d" },
+	graphql = { "eslint_d" },
 }
